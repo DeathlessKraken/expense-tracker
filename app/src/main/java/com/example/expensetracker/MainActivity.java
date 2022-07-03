58d8f7200e8614f5eab9aca2fde8bb4772c73ee3
Sean <michaelbrown.sean@gmail.com>
2022-07-02 20:09:41 -0700

    Added search bar and filters

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -8,10 +8,13 @@ import android.media.metrics.Event;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +22,72 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
 
+    private Button creditButton;
+    private Button debitButton;
+
+    private String selectedFilter = "all";
+    private String currentSearchText = "";
+    private SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupSearchWidget();
+
         setupData();
         setupList();
         setupOnClickListener();
 
+    }
+
+    private void setupSearchWidget()
+    {
+        searchView = (SearchView) findViewById(R.id.moneyEventListSearchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s)
+            {
+                currentSearchText = s;
+                ArrayList<MoneyEvent> filteredMoneyEvents = new ArrayList<MoneyEvent>();
+
+                for (MoneyEvent moneyEvent : moneyEventArrayList)
+                {
+                    if(moneyEvent.getNotes().toLowerCase().contains(s.toLowerCase()))
+                    {
+                        if(selectedFilter.equals("all"))
+                        {
+                            filteredMoneyEvents.add(moneyEvent);
+                        }
+                        else
+                        {
+                            if((moneyEvent.getType() == 0 ) && (selectedFilter.equals("credits")))
+                            {
+                                filteredMoneyEvents.add(moneyEvent);
+                            }
+                            if((moneyEvent.getType() == 1 ) && (selectedFilter.equals("debits")))
+                            {
+                                filteredMoneyEvents.add(moneyEvent);
+                            }
+                        }
+                    }
+                }
+
+                EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), 0, filteredMoneyEvents);
+                listView.setAdapter(eventAdapter);
+
+                return false;
+            }
+        });
     }
 
     private void setupData()
@@ -78,5 +137,57 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(showDetail);
             }
         });
+    }
+
+    private void filterList(String status)
+    {
+        selectedFilter = status;
+
+        ArrayList<MoneyEvent> filteredMoneyEvents = new ArrayList<MoneyEvent>();
+
+        for(MoneyEvent moneyEvent : moneyEventArrayList)
+        {
+            if((moneyEvent.getType() == 0 ) && (status.equals("credits")))
+            {
+                if(currentSearchText == "")
+                {
+                    filteredMoneyEvents.add(moneyEvent);
+                }
+                else
+                {
+                    if(moneyEvent.getNotes().toLowerCase().contains(currentSearchText.toLowerCase()))
+                    {
+                        filteredMoneyEvents.add(moneyEvent);
+                    }
+                }
+            }
+            if((moneyEvent.getType() == 1 ) && (status.equals("debits")))
+            {
+                filteredMoneyEvents.add(moneyEvent);
+            }
+        }
+
+        EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), 0, filteredMoneyEvents);
+        listView.setAdapter(eventAdapter);
+    }
+
+    public void creditFilterTapped(View view)
+    {
+        filterList("credits");
+    }
+
+    public void debitFilterTapped(View view)
+    {
+        filterList("debits");
+    }
+
+    public void allFilterTapped(View view)
+    {
+        selectedFilter = "all";
+        searchView.setQuery("", false);
+        searchView.clearFocus();
+
+        EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), 0, moneyEventArrayList);
+        listView.setAdapter(eventAdapter);
     }
 }
