79b646d2226a620bf175a1faaac7029055d5e298
Sean <michaelbrown.sean@gmail.com>
2022-07-03 18:30:41 -0700

    Added DatePicker button with todays date and max date

--- app/src/main/java/com/example/expensetracker/NewMoneyEventActivity.java
+++ app/src/main/java/com/example/expensetracker/NewMoneyEventActivity.java
@@ -2,31 +2,43 @@ package com.example.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class NewMoneyEventActivity extends AppCompatActivity {
 
     private Spinner categorySpinner;
     private Spinner typeSpinner;
 
-    private EditText amountText;
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_money_event);
-
+        
         Category.initCategoryList();
         Type.initTypeList();
 
+        initDatePicker();
+        
         categorySpinner = (Spinner) findViewById(R.id.categorySpinner);
         typeSpinner = (Spinner) findViewById(R.id.typeSpinner);
-        amountText = (EditText) findViewById(R.id.amountEditText);
+        dateButton = findViewById(R.id.datePickerButton);
+
+        dateButton.setText(getTodaysDate());
 
         CategorySpinnerAdapter categorySpinnerAdapter = new CategorySpinnerAdapter(this, R.layout.spinner_cell, Category.getCategoryList());
         categorySpinner.setAdapter(categorySpinnerAdapter);
@@ -35,6 +47,80 @@ public class NewMoneyEventActivity extends AppCompatActivity {
         TypeSpinnerAdapter typeSpinnerAdapter = new TypeSpinnerAdapter(this, R.layout.spinner_cell, Type.getTypeList());
         typeSpinner.setAdapter(typeSpinnerAdapter);
         typeSpinner.setSelection(1);
+    }
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        return makeDateString(day, month + 1, year);
+    }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+    }
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        return "BUN";
     }
 
     @Override
